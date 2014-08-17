fb97d25137791fe05f9300d1a220618646579739
Theo Kaloumenos <teokalu@gmail.com>
2014-08-17 12:31:30 +0200

    up & down buttons for total and sum-up times

--- timer.js
+++ timer.js
@@ -1,7 +1,7 @@
 //Here is all the logic
 
-var t_time_req = 0;
-var s_time_req = 0;
+var t_time_req;
+var s_time_req;
 var secondsRemaining;
 var intervalHandle;
 var publicTimerWindow;
@@ -13,6 +13,12 @@ $(document).ready(function() {
     //test1
     console.log("auto:document ready");
 
+    //reset req times
+    t_time_req = 0;
+    s_time_req = 0;
+    console.log("t_time: " + t_time_req);
+    console.log("s_time: " + s_time_req);
+
     //call resetDashboards()
     resetDashboards();
     console.log("auto:resetDashboards() run");
@@ -20,6 +26,10 @@ $(document).ready(function() {
     //call createButtonHandlers()
     createButtonHandlers();
     console.log("auto:createButtonHandlers() run");
+
+    /*//call openPublicTimerWindow()
+    openPublicTimerWindow()
+    console.log("auto:openPublicTimerWindow() run");*/
 });
 
 //start the timer in countdown mode
@@ -29,12 +39,6 @@ function startCountdown(){
 
     //get time from t_time_req
     var time = t_time_req;
-
-    // check if not a number
-    if (isNaN(time)){
-        alert("Please enter a number");
-        return; // stops function if true
-    }
 
     // how many seconds
     secondsRemaining = time * 60;
@@ -60,25 +64,30 @@ function tick(){
     //concatenate with colon
     var message = min.toString() + ":" + sec;
 
-    //change the display
-    //- in timer-admin
-    $('#A').html(message);
-
-    //- in timer-public
-    /*publicTimerWindow.document.getElementById("timer_public").textContent = message;*/
-
     //stop is down to zero
     if (secondsRemaining === 0){
 
         clearInterval(intervalHandle);
-
     }
 
     //subtract from seconds remaining
     secondsRemaining--;
 
+    //update displays
+    updateDisplays(message);
 }
 
+////update displays in admin and public views
+function updateDisplays(updatedTime) {
+
+    //- in timer-admin
+    $('#A').html(updatedTime);
+
+    //- in timer-public
+    if (publicTimerWindow) {
+        publicTimerWindow.document.getElementById("timer_public").textContent = updatedTime;
+    }
+}
 
 
 //opens a new window showing the public Timer
@@ -87,11 +96,12 @@ function openPublicTimerWindow() {
     //new window
     publicTimerWindow = window.open("timer-public.html", "publicTimerWin");
 
-
-
     console.log("openPublicTimerWindow() run...");
+}
 
-
+//closes the public Timer window
+function closePublicTimerWindow() {
+    publicTimerWindow.close();
 }
 
 //reset everything
@@ -132,7 +142,7 @@ function createButtonHandlers() {
     $('#b_s_up').attr('onclick', 'set_s_up()');
 
     //Sum-up time DOWN
-    $('#b_s_down').attr('onclick', 'set_t_down()');
+    $('#b_s_down').attr('onclick', 'set_s_down()');
 
     //START button
     $('#b_start').attr('onclick', 'startCountdown()');
@@ -143,68 +153,69 @@ function createButtonHandlers() {
     //CLEAR button
     $('#b_clear').attr('onclick', 'clear()');
 
-    //PublicTimer button
+    //PublicTimer open button
     $('#openPublic').attr('onclick', 'openPublicTimerWindow()');
+
+    //PublicTimer close button
+    $('#closePublic').attr('onclick', 'closePublicTimerWindow()');
 }
 
 function set_t_up() {
 
     //set total time up
-    t_time_req += 1;
+    if (t_time_req < 99) {
+        t_time_req += 1;
 
-    console.log(t_time_req);
-
-    //set A1 up
-    if (t_time_req < 10) {
-        $('#A1').html("0" + t_time_req + ":" + "00");
-    } else {
-        $('#A1').html(t_time_req + ":" + "00");
+        //set A1 up
+        if (t_time_req < 10) {
+            $('#A1').html("0" + t_time_req + ":" + "00");
+        } else {
+            $('#A1').html(t_time_req + ":" + "00");
+        }
     }
 }
 
 function set_t_down() {
 
     //set total time down
-    if (t_time_req > 0) {
+    if (t_time_req > 0 && t_time_req > s_time_req) {
         t_time_req -= 1;
-    }
-    console.log(t_time_req);
 
-    //set A1 down
-    if (t_time_req < 10) {
-        $('#A1').html("0" + t_time_req + ":" + "00");
-    } else {
-        $('#A1').html(t_time_req + ":" + "00");
+        //set A1 down
+        if (t_time_req < 10) {
+            $('#A1').html("0" + t_time_req + ":" + "00");
+        } else {
+            $('#A1').html(t_time_req + ":" + "00");
+        }
     }
 }
 
 function set_s_up() {
 
     //set sum-up time up
-    s_time_req += 1;
+    if (s_time_req < t_time_req) {
+        s_time_req += 1;
 
-    console.log(s_time_req);
-
-    //set A2 up
-    if (s_time_req < 10) {
-        $('#A2').html("0" + s_time_req + ":" + "00");
-    } else {
-        $('#A2').html(s_time_req + ":" + "00");
+        //set A2 up
+        if (s_time_req < 10) {
+            $('#A2').html("0" + s_time_req + ":" + "00");
+        } else {
+            $('#A2').html(s_time_req + ":" + "00");
+        }
     }
 }
 
-function set_t_down() {
+function set_s_down() {
 
     //set sum-up time down
     if (s_time_req > 0) {
         s_time_req -= 1;
-    }
-    console.log(s_time_req);
 
-    //set A2 down
-    if (s_time_req < 10) {
-        $('#A2').html("0" + s_time_req + ":" + "00");
-    } else {
-        $('#A2').html(s_time_req + ":" + "00");
+        //set A2 down
+        if (s_time_req < 10) {
+            $('#A2').html("0" + s_time_req + ":" + "00");
+        } else {
+            $('#A2').html(s_time_req + ":" + "00");
+        }
     }
 }
