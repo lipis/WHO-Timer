7d349c74cbf398a6e311384491d3f3e7519f9ad3
Theo Kaloumenos <teokalu@gmail.com>
2014-09-07 01:20:52 +0200

    no Ajax

--- timer-public.js
+++ timer-public.js
@@ -1,41 +1,8 @@
 /**
  * Created by Theo on 9/7/2014.
  */
-
-
 $(document).ready(function() {
-
-
-    /*$("#country_name").html("biiiipppp...");*/
-
-    myTrick = setInterval(checkAdmin, 1000);
-
 });
-
-function checkAdmin() {
-
-    /*$("#country_name").load("timer-admin.html #A");*/
-
-    /*jQuery.ajax({
-        type: 'get',
-        url: "timer-admin.html",
-        success: function(result) {
-            html = jQuery(result);
-            alert(html.find("div#template").html());
-            alert(html.find("div#template"));
-        }
-    });*/
-
-    $.get("timer-admin.html #A",function(){
-
-        /*window.opener.$("#A") = adminInput;*/
-
-        console.log("t!");
-        $("#country_name").html(window.opener.$("#A"));
-
-        });
-
-}
 
 
 
