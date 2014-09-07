713cdb3f15da0382a733d2ad41f445b9a7f434f4
Theo Kaloumenos <teokalu@gmail.com>
2014-09-07 02:37:31 +0200

    drag and drops

--- timer-admin.js
+++ timer-admin.js
@@ -331,3 +331,16 @@ $(document).ready(function() {
 
     myTimer.resetDisplays();
 });
+
+//speaker list code below this point:
+function drag(ev) {
+    ev.dataTransfer.setData("text/html", ev.target.id);
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text/html");
+    ev.target.appendChild(document.getElementById(data));
+}
